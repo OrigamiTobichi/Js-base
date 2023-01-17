9a056acf39f42988b899281afaf5b57d3947cb20
OrigamiTobichi <nguyenduc09012003@gmail.com>
2023-01-17 17:05:41 +0700

    first commit

---
+++ Js/main.js
@@ -0,0 +1,45 @@
+var a = 1;
+var b = 2;
+if (a == b) {
+  console.log("dk dung");
+} else {
+  console.log("dk sai");
+}
+
+var a = 1;
+var b = 2;
+/*Kết quả của phép so sánh chỉ trả về true or false*/
+var isSuccess = a > b;
+
+console.log(isSuccess);
+
+/*
+    - 0
+    - false
+    - ' ' - " "
+    - underfined
+    - NaN
+    - null
+    Đây là những giá trị false
+*/
+
+if (false) {
+  console.log("dk dung");
+} else {
+  console.log("dk sai");
+}
+
+var c = -1;
+
+if (!(a > 0 && b < 0) || c > 0) {
+  console.log("dung");
+}
+
+var isUnderfined;
+/* chỉ khai báo biến thì giá trị chả về sẽ là underfined*/
+console.log(isUnderfined);
+
+var isNull = null;
+console.log(isNull);
+
+console.log("hello");
